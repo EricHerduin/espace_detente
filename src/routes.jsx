23fad8b8e67a8bad90e6c6810502b79636db7bca
EricHerduin <ericherduin@icloud.com>
2022-11-10 16:55:29 +0100

    MAJ des routes sdu site

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,11 +3,12 @@ import Homepage from "./pages/homepage";
 import ErrorPage from "./pages/error-page";
 import Wellfare from "./pages/wellfare";
 import Product from "./pages/product";
+import Contact from "./pages/contact";
 import { createBrowserRouter } from "react-router-dom";
 
 const routing = createBrowserRouter([
   {
-    path: "/",
+    path: "Accueil",
     element: <Homepage />,
     errorElement: <ErrorPage />,
   },
@@ -18,6 +19,11 @@ const routing = createBrowserRouter([
   {
     path: "product/:_id",
     element: <Product />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "contact",
+    element: <Contact />,
     errorElement: <ErrorPage />,
   },
 ]);
